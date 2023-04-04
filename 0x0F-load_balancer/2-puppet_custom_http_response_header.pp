e7690eedd47dacb7f6f56e9fe8bad1f763fb0c87
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-04-04 01:00:42 +0000

    Puppet manifest that installs nginx with custome header.

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,17 @@
+# Adding custom header with puppet.
+# We automate the task using puppet.
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
